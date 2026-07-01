d157da9ab185e70753eee22d73aec578d9b7cc24
“asim-momin-7864” <asimmomin.dev@gmail.com>
2026-07-02 00:03:12 +0530

    Config DB

---
+++ src/config/db.config.ts
@@ -0,0 +1,14 @@
+//* db
+import mongoose from 'mongoose';
+import { env } from './env.config.js';
+import { baseLogger } from '#utils/logger.js';
+
+export const connectDB = async (): Promise<void> => {
+  try {
+    const conn = await mongoose.connect(env.MONGO_URL);
+    baseLogger.info(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    baseLogger.error(`MongoDB Connection Error: ${(error as Error).message}`);
+    process.exit(1);
+  }
+};
