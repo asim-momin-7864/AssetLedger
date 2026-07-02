1a97620ffdb53954506808f8735699a6c97f86ee
“asim-momin-7864” <asimmomin.dev@gmail.com>
2026-07-02 22:13:27 +0530

    Implement protectRoute middleware for JWT authentication

---
+++ src/middlewares/protectRoute.middleware.ts
@@ -0,0 +1,42 @@
+//* protect route
+import type { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import type { JwtPayload } from 'jsonwebtoken';
+import { env } from '#config/env.config.js';
+import { AppError } from '#errors/AppError.js';
+
+// shape of decoded JWT
+interface DecodedToken extends JwtPayload {
+  userId: string;
+}
+
+export const protectRoute = (req: Request, _res: Response, next: NextFunction): void => {
+  // extract token form cookies
+  const token = req.cookies?.jwt;
+
+  if (!token) {
+    throw new AppError('Unauthorized - Invalid Token Payload', 401);
+  }
+
+  try {
+    // verify
+    const decoded = jwt.verify(token, env.JWT_SECRET) as DecodedToken;
+
+    if (!decoded || !decoded.userId) {
+      throw new AppError('Unauthorized - Invalid Token Payload', 401);
+    }
+
+    // attched userId to request
+    req.user = {
+      _id: decoded.userId,
+    };
+
+    next();
+  } catch (error: unknown) {
+    if (error instanceof Error && error.name === 'TokenExpiredError') {
+      throw new AppError('Unauthorized - Token Expired', 401);
+    }
+
+    throw new AppError('Unauthorized - Invalid Token', 401);
+  }
+};
