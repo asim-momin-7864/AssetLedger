1dcfac7d221ec0b1e91539ecac0738516d1ccf16
“asim-momin-7864” <asimmomin.dev@gmail.com>
2026-07-03 00:18:37 +0530

    Implement dto for register and login input validation

---
+++ src/dtos/auth.dto.ts
@@ -0,0 +1,19 @@
+// dto for registration, login for validation parse
+import z from 'zod';
+
+// registration schema
+export const RegisterSchema = z.object({
+  name: z.string().min(2, 'Name must be at least 2 characters long').max(50),
+  email: z.email('Invalid email address format'),
+  password: z.string().min(6, 'Password must be at least 6 characters long'),
+});
+
+// login schema
+export const LoginSchema = z.object({
+  email: z.email('Invalid email address format'),
+  password: z.string().min(1, 'Password is required'),
+});
+
+// export derived typescript types
+export type RegisterInput = z.infer<typeof RegisterSchema>;
+export type LoginInput = z.infer<typeof LoginSchema>;
