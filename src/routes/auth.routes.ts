8a2300918bbb7c980122f800e5d61dc70caacb9e
“asim-momin-7864” <asimmomin.dev@gmail.com>
2026-07-05 01:20:40 +0530

    Change auth controller location by moving it into auth controller folder

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,6 +1,6 @@
 //* auth routes
 import { Router } from 'express';
-import { register, login, logout } from '#controllers/auth.controller.js';
+import { register, login, logout } from '#controllers/auth/auth.controller.js';
 import { protectRoute } from '#middlewares/protectRoute.middleware.js';
 import { authLimiter } from '#middlewares/rateLimiter.middleware.js';
 
