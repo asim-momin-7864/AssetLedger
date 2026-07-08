72e200a9069e32c7fa3818b14037c2f2474a71d6
“asim-momin-7864” <asimmomin.dev@gmail.com>
2026-07-08 22:42:59 +0530

    Implement action based business apis
    - Implement api to reject asset request
    - Implement api to fullfill asset request
    - Implement api to recover asset

---
+++ src/controllers/assetReq/assetReq.workflow.ts
@@ -0,0 +1,136 @@
+//* asset requests action base business logic controllers (fulfill, reject, recover)
+
+import { Asset } from '#models/asset.model.js';
+import { AssetReq } from '#models/assetReq.model.js';
+import { AppError } from '#errors/AppError.js';
+// import { getLogger } from 'pino-correlation-id';
+// import { baseLogger } from '#utils/logger.js';
+import type { Request, Response } from 'express';
+import { Types } from 'mongoose';
+
+// reject asset request
+export const rejectAssetReqController = async (req: Request, res: Response) => {
+  // params
+  const assetReqId = req.params.id;
+
+  //TODO status guard - check requests only be 'PENDING'
+  const assetReq = await AssetReq.findById(assetReqId);
+
+  if (!assetReq) {
+    throw new AppError('The request is not exists', 404);
+  }
+
+  if (assetReq?.status !== 'PENDING') {
+    throw new AppError('The request is already handled by ADMIN', 400);
+  }
+
+  // change
+  assetReq.status = 'REJECTED';
+  await assetReq.save();
+
+  res.status(200).json({
+    success: true,
+    message: `The Request:${assetReq.requestedItem} by ${assetReq.requesterId} is rejected by IT_ADMIN`,
+  });
+};
+
+// fullfill request (POST)
+export const fullfillAssetReqController = async (req: Request, res: Response) => {
+  // check params
+  const assetReqId = req.params.id;
+
+  //TEST
+
+  //TODO status guard - check requests only be 'PENDING'
+  const assetReq = await AssetReq.findById(assetReqId).populate('requesterId', ['name', 'email']);
+
+  console.log('assetReq ==> ', assetReq);
+
+  if (!assetReq) {
+    throw new AppError('The request is not exists', 404);
+  }
+
+  if (assetReq?.status !== 'PENDING') {
+    throw new AppError('The request is already handled by ADMIN', 400);
+  }
+
+  // check asset is available
+  const assetId = req?.body?.assetId;
+  // in this api request. body contain only assetId
+
+  if (!assetId) {
+    throw new AppError('The asset id is not found in asset request', 400);
+  }
+
+  // fetch asset
+  const asset = await Asset.findById(assetId);
+
+  // check
+  if (!asset) {
+    throw new AppError('The asset with provided ID dont exists', 404);
+  }
+
+  // check asset status
+  if (asset.status !== 'AVAILABLE') {
+    throw new AppError(
+      `The requested asset: ${asset.name} is not available, already assigned to employee: ${asset.assignedTo}`,
+      400
+    );
+  }
+
+  // values
+  const requesterId = assetReq.requesterId;
+
+  // update asset
+  asset.assignedTo = requesterId;
+  asset.status = 'ASSIGNED';
+  await asset.save();
+
+  // update asset request
+  assetReq.status = 'APPROVED';
+  await assetReq.save();
+
+  // populated user
+  interface PopulatedUser {
+    _id: Types.ObjectId;
+    name: string;
+    email: string;
+  }
+
+  const requester = assetReq.requesterId as unknown as PopulatedUser;
+
+  // res
+  res.status(200).json({
+    success: true,
+    message: `The asset request for Asset: ${asset.name} is approved, and assigned to Employee:${requester.email}`,
+  });
+};
+
+// recover asset
+export const recoverAssetController = async (req: Request, res: Response) => {
+  // values
+  const assetId = req.params.assetId; // in params we get assetId
+
+  // check
+  const assetExist = await Asset.findById(assetId);
+
+  if (!assetExist) {
+    throw new AppError('The asset to recover is not exist', 404);
+  }
+
+  // its status
+  if (assetExist.status !== 'ASSIGNED' || assetExist.assignedTo === null) {
+    throw new AppError('The asset cannot get recover it is available or in maintaince', 400);
+  }
+
+  // change
+  assetExist.assignedTo = null;
+  assetExist.status = 'AVAILABLE';
+  await assetExist.save();
+
+  // res
+  res.status(200).json({
+    succes: true,
+    message: `The asset: ${assetExist.name} and serial number ${assetExist.serialNumber} is successfully recovered`,
+  });
+};
--- src/routes/assetReq.routes.ts
+++ src/routes/assetReq.routes.ts
@@ -5,19 +5,35 @@ import {
   allAssetReqsAdminController,
   myAssetRequestsController,
 } from '#controllers/assetReq/assetReq.controllers.js';
+import {
+  rejectAssetReqController,
+  fullfillAssetReqController,
+  recoverAssetController,
+} from '#controllers/assetReq/assetReq.workflow.js';
 import { protectRoute } from '#middlewares/protectRoute.middleware.js';
 import { authorizeRoles } from '#middlewares/authorizeRoles.middleware.js';
 
 // insatnce
 const router = Router();
 
+router.use(protectRoute);
+
 //  create
-router.post('/', protectRoute, createAssetReqController);
+router.post('/', createAssetReqController);
 
 // all requests
-router.get('/', protectRoute, authorizeRoles('IT_ADMIN'), allAssetReqsAdminController);
+router.get('/', authorizeRoles('IT_ADMIN'), allAssetReqsAdminController);
 
 // my requests
-router.get('/my-requests', protectRoute, myAssetRequestsController);
+router.get('/my-requests', myAssetRequestsController);
+
+// reejct asset reqeust
+router.patch('/:id/reject', authorizeRoles('IT_ADMIN'), rejectAssetReqController);
+
+// fullfill asset request
+router.patch('/:id/fullfill', authorizeRoles('IT_ADMIN'), fullfillAssetReqController);
+
+// recover asset
+router.patch('/:assetId/recover', authorizeRoles('IT_ADMIN'), recoverAssetController);
 
 export default router;
