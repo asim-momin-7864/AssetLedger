e3ae108457fa6d41f18c02ebc6cfc851413a7cd1
“asim-momin-7864” <asimmomin.dev@gmail.com>
2026-07-08 19:43:36 +0530

    Implement two apis for asset request
    - 1st for admin to get all requests
    - 2nd for employee to get all his requests

--- src/controllers/assetReq/assetReq.controllers.ts
+++ src/controllers/assetReq/assetReq.controllers.ts
@@ -6,7 +6,7 @@ import { CreateAssetReqSchema, CreateAssetReqInput } from '#dtos/assetReq.dto.js
 import { getLogger } from 'pino-correlation-id';
 import { baseLogger } from '#utils/logger.js';
 
-// create asset req controller - employee
+// create asset req controller -- EMPLOYEE
 export const createAssetReqController = async (
   req: Request<unknown, unknown, CreateAssetReqInput>,
   res: Response
@@ -60,5 +60,55 @@ export const createAssetReqController = async (
   res.status(200).json({
     success: true,
     data: newAssetReq,
+  });
+};
+
+// all asset requests -- IT ADMIN
+
+// types assertion for query
+// status values
+type AssetReqStatus = 'PENDING' | 'APPROVED' | 'REJECTED';
+
+// type guarde
+const isValideStatus = (status: unknown): status is AssetReqStatus => {
+  return typeof status === 'string' && ['PENDING', 'APPROVED', 'REJECTED'].includes(status);
+};
+
+export const allAssetReqsAdminController = async (req: Request, res: Response) => {
+  // query
+  const { status } = req.query;
+
+  // filter object
+  const filter: { status?: AssetReqStatus } = {};
+
+  // check
+  if (isValideStatus(status)) {
+    filter.status = status;
+  }
+
+  const allRequests = await AssetReq.find(filter);
+
+  // res
+  res.status(200).json({
+    success: true,
+    data: allRequests,
+  });
+};
+
+// my requests -- EMPLOYEE
+export const myAssetRequestsController = async (req: Request, res: Response) => {
+  const requesterId = req.user?._id;
+
+  // check user
+  if (!requesterId) {
+    throw new AppError('UnAuthorized or User not exists', 401);
+  }
+
+  const myRequests = await AssetReq.find({ requesterId: requesterId });
+
+  // res
+  res.status(200).json({
+    success: true,
+    data: myRequests,
   });
 };
--- src/routes/assetReq.routes.ts
+++ src/routes/assetReq.routes.ts
@@ -1,12 +1,23 @@
 //* asset request routes
 import { Router } from 'express';
-import { createAssetReqController } from '#controllers/assetReq/assetReq.controllers.js';
+import {
+  createAssetReqController,
+  allAssetReqsAdminController,
+  myAssetRequestsController,
+} from '#controllers/assetReq/assetReq.controllers.js';
 import { protectRoute } from '#middlewares/protectRoute.middleware.js';
-// import { authorizeRoles } from '#middlewares/authorizeRoles.middleware.js';
+import { authorizeRoles } from '#middlewares/authorizeRoles.middleware.js';
 
 // insatnce
 const router = Router();
 
+//  create
 router.post('/', protectRoute, createAssetReqController);
+
+// all requests
+router.get('/', protectRoute, authorizeRoles('IT_ADMIN'), allAssetReqsAdminController);
+
+// my requests
+router.get('/my-requests', protectRoute, myAssetRequestsController);
 
 export default router;
