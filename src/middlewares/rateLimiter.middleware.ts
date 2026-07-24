e352cf53ae452d8a129f36c40eb41b9d91cf4a2d
“asim-momin-7864” <asimmomin.dev@gmail.com>
2026-07-24 17:22:24 +0530

    fix: Fix rate middleware env config

--- src/middlewares/rateLimiter.middleware.ts
+++ src/middlewares/rateLimiter.middleware.ts
@@ -1,10 +1,12 @@
 //* rateLimiter
 import rateLimit from 'express-rate-limit';
+import { env } from '#config/env.config.js';
 
 // front gate api rate limiter
 export const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 min
   max: 100,
+  skip: () => env.NODE_ENV === 'test',
   standardHeaders: true,
   legacyHeaders: false,
   message: {
@@ -17,6 +19,7 @@ export const apiLimiter = rateLimit({
 export const authLimiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 1 hr
   max: 5,
+  skip: () => env.NODE_ENV === 'test',
   message: {
     success: false,
     message: 'Too many login attempts from this IP, please try again after an hour',
